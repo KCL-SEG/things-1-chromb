0bddeab99f955adc231f10903a4ad75c35242db5
chromb <94870563+chromb@users.noreply.github.com>
2023-12-07 23:58:52 +0000

    Revert "Revert "fix capitalization of /templates""
    This reverts commit bf5e3c77f99761559b4471d1502432e1e572d224.

--- things/models.py
+++ things/models.py
@@ -4,8 +4,14 @@ from django.core.validators import MaxValueValidator
 
 # Create your models here.
 
+'''
 class Thing(models.Model):
-    name = models.charField(max_length=30, unique=True, blank=False)
+    name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.TextField(max_length=120)
     quantity = models.IntegerField(models.CheckConstraint(MaxValueValidator(100), MinValueValidator(0)))
-    
+'''
+
+class Thing(models.Model):
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.TextField(max_length=120)
+    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
