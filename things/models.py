5582a10e72771cd51d1b5af7ffe02891c5666aec
chromb <ge11684@outlook.com>
2023-12-08 00:07:32 +0000

    h

--- things/models.py
+++ things/models.py
@@ -12,6 +12,6 @@ class Thing(models.Model):
 '''
 
 class Thing(models.Model):
-    name = models.CharField(max_length=30, unique=True, blank=False)
-    description = models.TextField(max_length=120)
-    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    name = models.CharField()
+    description = models.TextField()
+    quantity = models.IntegerField()
